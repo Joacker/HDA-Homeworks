b60bd6188d56f4ab7085de9bd5e9e1d91ee47720
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-09-30 01:48:11 -0300

    constraints working on

--- backend/models/Resenias.go
+++ backend/models/Resenias.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Resenias struct {
 	gorm.Model
 
-	Id        int    //`gorm:"not null"` // ID is the primary key
-	Id_agente int    //`gorm:"not null"` // ID is the primary key
-	Resenia   string //`gorm:"not null"` // Title is the item title
-	Id_user   int    //`gorm:"not null"` // Description is the item descriptions
+	Id      int    `gorm:"not null","autoIncrement"` // ID is the primary key
+	Resenia string //`gorm:"not null"` // Title is the item title
+	Users   Users  `gorm:"foreignKey:Id"`
+	Agents  Agents `gorm:"foreignKey:Id"`
 }
