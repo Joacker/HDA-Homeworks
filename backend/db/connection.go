ac0fd6eb448d3c95540fae5a4e06ac6516f7948f
Joaquín Fernández <67400456+Joacker@users.noreply.github.com>
2022-10-15 11:48:59 -0300

    .env added in backend

--- backend/db/connection.go
+++ backend/db/connection.go
@@ -1,16 +1,29 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DSN = "host=postgres user=postgres dbname=tarea1 password=postgres sslmode=disable port=5432 TimeZone=America/Santiago"
 var DB *gorm.DB
 
 func DBConnection() {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
+	user := os.Getenv("POSTGRESQL_USER")
+	password := os.Getenv("POSTGRESQL_PASSWORD")
+	host := os.Getenv("POSTGRESQL_HOST")
+	port := os.Getenv("POSTGRESQL_PORT")
+	dbname := os.Getenv("POSTGRESQL_DATABASE")
+	var DSN = "host" + "=" + host + " user" + "=" + user + " dbname" + "=" + dbname + " password" + "=" + password + " sslmode=disable" + " port" + "=" + port + " TimeZone=America/Santiago"
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
@@ -18,4 +31,5 @@ func DBConnection() {
 	} else {
 		log.Println("DB connected")
 	}
+
 }
--- backend/db/go_env.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import (
-
-)
-
-func loadEnv() {
-	
-}
